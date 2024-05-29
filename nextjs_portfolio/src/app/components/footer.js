2df02a326b78c960ab73181fa5f36f30731efb1a
Aditya Raj <adistrim.dev@gmail.com>
2024-05-29 17:27:35 +0530

    changed the design

--- nextjs_portfolio/src/app/components/footer.js
+++ nextjs_portfolio/src/app/components/footer.js
@@ -1,58 +1,46 @@
 import Link from 'next/link';
-import { FaLinkedinIn, FaGithub, FaTwitter } from 'react-icons/fa';
+import { FaLinkedinIn, FaGithub, FaKaggle } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-[#D1ADEE] text-[#544274] py-8">
-      <div className="container mx-auto px-6">
+    <footer className="py-8">
+      <div className="container mx-[5rem] px-6">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="mb-4 md:mb-0">
-            <Link href="/">
-              <span className="text-2xl font-bold cursor-pointer">Diya Mathur</span>
-            </Link>
+          <div className="tracking-widest font-semibold hover:text-[#68B3A3] ">
+            <Link href="/">HOME</Link>
           </div>
           <nav>
-            <ul className="flex flex-col md:flex-row">
-              <li className="mb-2 md:mb-0 md:mr-6">
-                <Link href="/projects">
-                  <span className="hover:text-[#8C58D3] transition duration-300">Projects</span>
-                </Link>
-              </li>
-              <li className="mb-2 md:mb-0 md:mr-6">
-                <Link href="/about">
-                  <span className="hover:text-[#8C58D3] transition duration-300">About</span>
-                </Link>
-              </li>
-              <li className="mb-2 md:mb-0 md:mr-6">
-                <Link href="/resume">
-                  <span className="hover:text-[#8C58D3] transition duration-300">Resume</span>
-                </Link>
-              </li>
-              <li className="mb-2 md:mb-0 md:mr-6">
-                <Link href="/blogs">
-                  <span className="hover:text-[#8C58D3] transition duration-300">Blogs</span>
-                </Link>
-              </li>
-              <li className="mb-2 md:mb-0">
-                <Link href="/certifications">
-                  <span className="hover:text-[#8C58D3] transition duration-300">Certifications</span>
-                </Link>
-              </li>
-            </ul>
+            <div className="flex flex-col md:flex-row space-x-5">
+              <Link href="#intro" className="hover:text-[#68B3A3]">
+                INTRO
+              </Link>
+              <Link href="#projects" className="hover:text-[#68B3A3]">
+                PROJECTS
+              </Link>
+              <Link href="#blogs" className="hover:text-[#68B3A3]">
+                BLOGS
+              </Link>
+              <Link href="" className="hover:text-[#68B3A3]">
+                RESUME
+              </Link>
+              <Link href="#certifications" className="hover:text-[#68B3A3]">
+                CERTIFICATIONS
+              </Link>
+            </div>
           </nav>
-          <div className="flex items-center mt-4 md:mt-0">
-            <Link href="https://www.linkedin.com/in/diyamth29/" className="mr-4 hover:text-[#8C58D3] transition duration-300">
+          <div className="flex items-center space-x-5 mt-4 md:mt-0">
+            <Link href="https://www.linkedin.com/in/diyamth29/" target="_blank" rel="noopener noreferrer" className="hover:text-[#68B3A3]">
               <FaLinkedinIn size={24} />
             </Link>
-            <Link href="https://github.com/diyamth" className="mr-4 hover:text-[#8C58D3] transition duration-300">
+            <Link href="https://github.com/diyamth" target="_blank" rel="noopener noreferrer" className="hover:text-[#68B3A3]">
               <FaGithub size={24} />
             </Link>
-            <Link href="" className="hover:text-[#8C58D3] transition duration-300">
-              <FaTwitter size={24} />
+            <Link href="https://www.kaggle.com/diyamathur" target="_blank" rel="noopener noreferrer" className="hover:text-[#68B3A3]">
+              <FaKaggle size={24} />
             </Link>
           </div>
         </div>
-        <div className="mt-8 text-center">
+        <div className="mt-[5rem] text-center">
           <p>&copy; {new Date().getFullYear()} Diya Mathur. All rights reserved.</p>
         </div>
       </div>
--- nextjs_portfolio/src/app/components/header.js
+++ nextjs_portfolio/src/app/components/header.js
@@ -1,30 +1,27 @@
 import Link from 'next/link';
-import { Syncopate } from 'next/font/google';
-
-const syncopate = Syncopate({ subsets: ['latin'], weight: ['400', '700'] });
 
 const Header = () => {
   return (
-    <header className="bg-[#E0CFEE] border-b border-[#E5E1E6] text-black">
-      <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        <div className={`text-[2rem] font-thin tracking-widest ${syncopate.className}`}>
-          <Link href="/">DIYA</Link>
+    <header className="my-5 text-black" style={{ position: "sticky", top: 0, width: "100%", zIndex: 100, backgroundColor: "transparent" }}>
+      <div className="container mx-[5rem] flex justify-between items-center py-4 px-6">
+        <div className="tracking-widest font-semibold hover:text-[#68B3A3] ">
+          <Link href="/">HOME</Link>
         </div>
-        <nav className="space-x-[3rem] font-light underline-offset-[2.1rem]">
-          <Link href="/projects" className="hover:text-white hover:underline">
-            Projects
+        <nav className="space-x-[3rem] font-light">
+          <Link href="#intro" className="hover:text-[#68B3A3]">
+            INTRO
           </Link>
-          <Link href="/about" className="hover:text-white hover:underline">
-            About
+          <Link href="#projects" className="hover:text-[#68B3A3]">
+            PROJECTS
           </Link>
-          <Link href="/resume" className="hover:text-white hover:underline">
-            Resume
+          <Link href="#blogs" className="hover:text-[#68B3A3]">
+            BLOGS
           </Link>
-          <Link href="/blogs" className="hover:text-white hover:underline">
-            Blogs
+          <Link href="" className="hover:text-[#68B3A3]">
+            RESUME
           </Link>
-          <Link href="/certifications" className="hover:text-white hover:underline">
-            Certifications
+          <Link href="#certifications" className="hover:text-[#68B3A3]">
+            CERTIFICATIONS
           </Link>
         </nav>
       </div>
