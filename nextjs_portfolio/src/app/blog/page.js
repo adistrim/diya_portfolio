e839ee63b9af442a8c9a398b2d6282aaa6fe3d5c
Aditya Raj <adistrim.dev@gmail.com>
2024-05-30 17:31:35 +0530

    Deployment issue #1: fixed

--- nextjs_portfolio/src/app/blog/page.js
+++ nextjs_portfolio/src/app/blog/page.js
@@ -1,6 +1,6 @@
 const Blog = () => {
     return (
-        <h1 className="text-md md:text-2xl lg:text-4xl">I'm blog</h1>
+        <h1 className="text-md md:text-2xl lg:text-4xl">I&apos;m blog</h1>
     );
 };
 
