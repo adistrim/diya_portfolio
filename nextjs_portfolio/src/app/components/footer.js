b0d335b6b1fa930481205c1713f7d0cd516a7efe
adistrim <adistrim.dev@gmail.com>
2024-06-05 12:41:32 +0530

    made scrolling smooth accross the links

--- nextjs_portfolio/src/app/components/footer.js
+++ nextjs_portfolio/src/app/components/footer.js
@@ -1,28 +1,47 @@
+"use client";
 import Link from 'next/link';
 import { FaLinkedinIn, FaGithub, FaKaggle } from 'react-icons/fa';
 
 const Footer = () => {
+
+  const handleScroll = (e, id) => {
+    e.preventDefault();
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+      setIsOpen(false);
+    }
+  };
+
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
   return (
     <footer className="py-8">
       <div className="container mx-auto px-6 md:px-12">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="tracking-widest font-semibold hover:text-[#68B3A3] mb-4 md:mb-0">
-            <Link href="/">HOME</Link>
+            <p className='cursor-pointer' onClick={scrollToTop} >HOME</p>
           </div>
           <nav className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-5 mb-4 md:mb-0">
-            <Link href="#intro" className="hover:text-[#68B3A3] text-center md:text-left">
+            <Link
+              href="#intro"
+              className={`hover:text-[#68B3A3] text-center md:text-left`}
+              onClick={(e) => handleScroll(e, 'intro')}
+            >
               INTRO
             </Link>
-            <Link href="#projects" className="hover:text-[#68B3A3] text-center md:text-left">
+            <Link href="#projects" className="hover:text-[#68B3A3] text-center md:text-left" onClick={(e) => handleScroll(e, 'projects')}>
               PROJECTS
             </Link>
-            <Link href="#blogs" className="hover:text-[#68B3A3] text-center md:text-left">
+            <Link href="#blogs" className="hover:text-[#68B3A3] text-center md:text-left" onClick={(e) => handleScroll(e, 'blogs')}>
               BLOGS
             </Link>
             <Link href="" className="hover:text-[#68B3A3] text-center md:text-left">
               RESUME
             </Link>
-            <Link href="#certifications" className="hover:text-[#68B3A3] text-center md:text-left">
+            <Link href="#certifications" className="hover:text-[#68B3A3] text-center md:text-left" onClick={(e) => handleScroll(e, 'certifications')}>
               CERTIFICATIONS
             </Link>
           </nav>
